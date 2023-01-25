eea8bb848667fa022aa7ef3408d47d4dd00f3a08
eunjng5474 <eunjng5474@gmail.com>
2023-01-25 17:26:32 +0900

    ws commit - 230125 0526

--- HWS/데일리+실습_python_06_1_P.py
+++ HWS/데일리+실습_python_06_1_P.py
@@ -2,23 +2,62 @@
 # 주민등록번호를 입력받아 주민등록번호 뒷자리를 비식별화하는 함수 de_identify 를 만들어라.
 
 
-def de_identify(number):
-    # num = number.replace(number[-7:-1], '*')
-    # return num
-    # num = number.strip('-')
-    # num = num.replace(num[-7], '*', 7)
-    # return num
-    for n in range(-7, 0):
-        num = number.replace(number[n], '*')
-    return num
-    #     # print(number[-n])
-    # return 
+# def de_identify(number):
+#     # num = number.replace(number[-7:-1], '*')
+#     # return num
+#     # num = number.strip('-')
+#     # num = num.replace(num[-7], '*', 7)
+#     # return num
+#     for n in range(-7, 0):
+#         num = number.replace(number[n], '*')
+#     return num
+#     #     # print(number[-n])
+#     # return 
+
+
+
+
+############# sol
+# 해당 변수는 인자를 하나만 받는다.
+# 함수 정의시 매개변수는 한 개면 된다.
+def de_identify(string):
+    # 문자열을 받아온 다음, 그 문자열이 가진 내용 중
+    # '-'에 해당하는 부분을 없앨 것이다.
+    # 반복문을 통해서 넘겨받은 문자열을 하나하나 비교한 뒤,
+    # 특정 문자열을 제외한 문자열을 만들어서 반환해야 한다.
+    result = ''
+    # for char in string:
+    #     if char == '-':
+    #         continue
+    #     else:
+    #         result += char
+    result = string.replace('-', '')     # 문자열이 가진 특성상 원본을 바꿀 수는 없으니 변경된 값을 반환
+    
+    # print(result[:6] + '*'*7)     # '970103*******'   # 이런식으로도 가능
+    
+    result = list(result)
+    result[6:] = ['*']*7
+    return ''.join(result)
+    # print(result)
+    # real_result = ''
+    # idx = 0
+    # for char in result:
+    #     idx += 1
+    #     if idx >= 6:
+    #         real_result += '*'
+    #     else:
+    #         real_result += char
+    #     idx += 1
+
+
+
+
+
 
 # A. 입력예시
 print(de_identify('970103-1234567'))
-# print(de_identify('8611232345678'))
+print(de_identify('8611232345678'))
 
 # B. 출력예시
 # 970103*******
 # 861123******* 
-
--- HWS/데일리+실습_python_06_2_P.py
+++ HWS/데일리+실습_python_06_2_P.py
@@ -4,9 +4,31 @@
 
 grain_lst = [('고구마',3000), ('감자',2000), ('옥수수',4500),('토란',1300)]
 
-max_p = 0
-for i in range(len(grain_lst)):
-    if grain_lst[i][-1] > max_p:
-        max_p = grain_lst[i][-1]
+
+# max_p = 0
+# for i in range(len(grain_lst)):
+#     if grain_lst[i][-1] > max_p:
+#         max_p = grain_lst[i][-1]
         
     
+
+# #################### sol 
+# # print(grain_lst.sort()) # None 반환
+# # print(grain_lst) # 정렬된 리스트 확인
+# grain_dict = dict(grain_lst)
+# key = list(grain_dict.keys())
+# value = list(grain_dict.values())
+# # print(key, value)
+# # print(value.index(4500))
+# # print(value.index(max(value))) # 2
+# max_idx = value.index(max(value))
+# print(key[max_idx])
+
+
+
+# 2
+grain_lst.sort(key=lambda x : x[1])
+print(grain_lst[-1][0])
+
+grain_lst.sort(key=lambda x : x[1], reverse=True)
+print(grain_lst[0][0])
